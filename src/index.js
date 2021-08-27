0f63d049b3f8154e09f0642cb97f09bcd2cce70e
sridevi14 <90skidcartoon@gmail.com>
2021-08-27 21:52:57 +0530

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+
+import NavBar from './components/Header'; 
+import Excercise from './components/Excercise'; 
+import Diet from './components/Diet'; 
+import Sign_up from './components/Sign_up';
+import Coach from './components/Coach'; 
+import Apps from './components/mobile'; 
+
+import './components/App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from './components/About';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+ <Router>
+        <NavBar/>
+
+        <div className="pages">
+          <Switch>
+            <Route exact path="" component={Apps} />
+            <Route path="/about" component={About} />
+            <Route path="/Coach" component={Coach} />
+            <Route path="/Diet" component={Diet} />
+            <Route path='/Excercise' component={Excercise}/>
+<Route path='/Sign_up' component={Sign_up}/>
+          </Switch>
+        </div>
+      </Router>
+     </div>
+  );
+}
+
+
 
 
 ReactDOM.render(
